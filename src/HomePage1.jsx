21fe7f9d62e258af5ff2cf45ac074d9d6be1efa3
basmah yaqoob89 <basmahyaqoob89@gmail.com>
2026-02-16 11:15:16 +0300

    update

--- src/HomePage1.jsx
+++ src/HomePage1.jsx
@@ -1,17 +1,17 @@
 
-import Card1 from "./card1";
-import Card2 from "./Card2";
-import Card3 from "./Card3";
-import Card4 from "./Card4";
-import Card5 from "./Card5";
-import Card6 from "./Card6";
-import Card7 from "./Card7";
-import Card8 from "./Card8";
-import Card9 from "./Card9";
-import Card10 from "./Card10";
-import Card11 from "./Card11";
-import Card12 from "./Card12";
-import Card13 from "./Card13";
+import Card1 from "./Welcome";
+import Card2 from "./QuickDraft";
+import Card3 from "./YearlyTargets";
+import Card4 from "./TicketsStatistics";
+import Card5 from "./LatestNews";
+import Card6 from "./LatestTasks";
+import Card7 from "./TopSearchItems";
+import Card8 from "./LatestUploads";
+import Card9 from "./LastProjectProgress";
+import Card10 from "./Reminders";
+import Card11 from "./LatestPost";
+import Card12 from "./SocialMediaStats";
+import Card13 from "./Projects";
 import Layout1 from "./Layout1";
 
 function Cards(){
